6c029a4c4508d43753e714f18db650481af2486b
creasy-pita <hz_ljq@live.com>
2023-11-12 19:23:18 +0800

    1 springboot程序集成activiti-5.18.0 + shardingshpere-4.x

--- modules/shardingsphereDemo/src/test/java/com/creaypita/activiti/DemoTest.java
+++ modules/shardingsphereDemo/src/test/java/com/creaypita/activiti/DemoTest.java
@@ -3,6 +3,8 @@ package com.creaypita.activiti;
 import com.creasypita.activiti.DemoApplication;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +22,29 @@ public class DemoTest {
 //    public ProcessEngine processEngine;
     public RepositoryService repositoryService;
 
+//    @Test
+//    public void showservice(){
+////        RepositoryService repositoryService = processEngine.getRepositoryService();
+//        System.out.println(repositoryService);
+//        repositoryService.
+//    }
     @Test
-    public void showservice(){
-//        RepositoryService repositoryService = processEngine.getRepositoryService();
-        System.out.println(repositoryService);
+    public void getProcessDefinition() {
+
+        Deployment deployment = repositoryService.createDeployment()
+//                .addClasspathResource("second_approve.bpmn20.xml")
+                .addClasspathResource("process.bpmn20.xml")
+                .deploy();
+
+        //部署id
+        String deploymentId = deployment.getId();
+
+        //流程对应对象
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deploymentId)
+                .singleResult();
+        System.out.println(String.format( "流程名称 [%s],流程ID [%s],流程KEY [%s]",processDefinition.getName(),processDefinition.getId(),processDefinition.getKey()));
+//        logger.info("流程名称 [{}],流程ID [{}],流程KEY [{}]",processDefinition.getName(),processDefinition.getId(),processDefinition.getKey());
+//        return processDefinition;
     }
 }
